7de2c31da88af539a971088452d7910046037be0
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-01-12 20:52:38 +0100

    beggining Aurora Forecast porject backend setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
 import app from "./app";
+import env from "./utils/validateEnv";
+import redis from "redis";
+
+const port = env.PORT;
+const redisClient = redis.createClient();
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+redisClient
+  .connect()
+  .then(() => {
+    console.log("Redis connected");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(console.error);
