3ead4ca8a0be6a40b6d57857b0777e01a1981527
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-05-20 23:26:33 +0200

    activated cors

--- dist/createApp.js
+++ dist/createApp.js
@@ -11,9 +11,11 @@ const morgan = require("morgan");
 function createApp() {
     const app = (0, express_1.default)();
     app.use((0, cors_1.default)());
-    app.use((0, cors_1.default)({
-        origin: "https://auroraforecast.online/",
-    }));
+    // app.use(
+    //     cors({
+    //         origin: "https://auroraforecast.online/",
+    //     })
+    // );
     app.use(express_1.default.json());
     app.use(express_1.default.static("public"));
     app.use(morgan("dev"));
